9ae2058598df8c9a55879997ba1f779876217a97
ishiishikou <kou.ishi@gmail.com>
2026-07-05 00:53:35 +0900

    Step 2: CDK assertionsテストを追加

---
+++ test/ec2-stack.test.ts
@@ -0,0 +1,48 @@
+import * as cdk from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import { Ec2Stack } from '../lib/ec2-stack';
+
+const createTemplate = (): Template => {
+  const app = new cdk.App();
+  const stack = new Ec2Stack(app, 'TestEc2Stack');
+
+  return Template.fromStack(stack);
+};
+
+describe('Ec2Stack', () => {
+  test('creates one EC2 instance', () => {
+    const template = createTemplate();
+
+    template.resourceCountIs('AWS::EC2::Instance', 1);
+  });
+
+  test('does not add inbound rules to the security group', () => {
+    const template = createTemplate();
+
+    template.resourceCountIs('AWS::EC2::SecurityGroupIngress', 0);
+    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
+      SecurityGroupIngress: Match.absent(),
+    });
+  });
+
+  test('attaches the SSM managed policy to the EC2 role', () => {
+    const template = createTemplate();
+
+    template.hasResourceProperties('AWS::IAM::Role', {
+      ManagedPolicyArns: Match.arrayWith([
+        {
+          'Fn::Join': [
+            '',
+            [
+              'arn:',
+              {
+                Ref: 'AWS::Partition',
+              },
+              ':iam::aws:policy/AmazonSSMManagedInstanceCore',
+            ],
+          ],
+        },
+      ]),
+    });
+  });
+});
