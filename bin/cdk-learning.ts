0b1e5a03887efbeaff02fe6f793c7169155d5370
ishiishikou <kou.ishi@gmail.com>
2026-07-05 00:11:09 +0900

    Step 1: Ec2Stackへ変更

--- bin/cdk-learning.ts
+++ bin/cdk-learning.ts
@@ -1,12 +1,6 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { SsmEc2Stack } from '../lib/ssm-ec2-stack';
+import { Ec2Stack } from '../lib/ec2-stack';
 
 const app = new cdk.App();
 
-new SsmEc2Stack(app, 'CdkLearningSsmEc2Stack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+new Ec2Stack(app, 'Ec2Stack');
